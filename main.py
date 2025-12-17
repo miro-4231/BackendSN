7a82d1d6b7eeb6f89dd6952dcf4f46f6d6bfe727
miro-4231 <131700226+miro-4231@users.noreply.github.com>
2025-12-17 20:02:30 +0100

    learning additional stuff related to webDev

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
+from random import randint
 
 app = FastAPI() 
 
-@app.get("/")
-async def root():
-    return {"message" : "Welcome to my API"}
+class Post(BaseModel):
+    title: str 
+    content: str
+    rating: Optional[int] = None
+
+my_posts = [{"title":"title of post 1", "content":"content of post 1", "id":1}, 
+            {"title":"fav food", "content":"I like pizza", "id":2}]
+
+def find_post(id:int):
+    for post in my_posts:
+        if post["id"] == id: 
+            return post
+
 
 @app.get("/posts")
-def get_posts():
-    return {"data" : "This is your posts"}
+async def root():
+    return {"data" : my_posts}
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"meassage":"opperation succesfull"}
+@app.get("/posts/latest")
+def get_posts():
+    return {"data" : my_posts[-1]}
+
+@app.get("/posts/{id}")
+def get_posts(id:int):
+    post = find_post(id)
+    if not post: 
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'post with id: {id} not found.')
+    return {"data" : post}
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+def create_posts(post:Post):
+    post_dict = post.dict()
+    post_dict["id"] = randint(0, 10**5)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
